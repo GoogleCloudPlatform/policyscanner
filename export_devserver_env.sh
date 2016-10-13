90911cf1fbe8e9d655990c80abd31ee1f4c1bd1b
Carise F <carise@users.noreply.github.com>
2016-10-13 13:20:04 -0700

    Add documentation in env script and check for xmlstarlet dependency (#8)

--- export_devserver_env.sh
+++ export_devserver_env.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Run this script to export the appengine-web.xml env variables
+# Source this script to export the appengine-web.xml env variables
 # as your local machine's env variables. This is used for running
 # the local devserver.
+#
+# Be sure to set appengine-web.xml POLICY_SCANNER_EXECUTE_ON_CLOUD
+# to FALSE.
+#
+# Usage:
+#
+# . ./export_devserver_env.sh
+
+if ! command -v xmlstarlet >/dev/null; then
+  echo "This script depends on xmlstarlet, but I don't see it."
+  echo "Maybe you should either install it or include its location" \
+    "in your PATH environment variable."
+  return 1
+fi
 
 while read -r line; do
   env_var=$(echo ${line} | cut -f 1 -d '=')
