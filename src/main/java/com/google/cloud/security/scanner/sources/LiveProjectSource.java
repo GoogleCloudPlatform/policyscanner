1c9c5c3baa905437e6efeedaddead2f83d88875c
Carise Fernandez <carise@google.com>
2016-10-25 16:56:38 -0700

    Fix the null/deleted project check

--- src/main/java/com/google/cloud/security/scanner/sources/LiveProjectSource.java
+++ src/main/java/com/google/cloud/security/scanner/sources/LiveProjectSource.java
@@ -33,6 +33,7 @@ import java.util.NoSuchElementException;
 /** Source for listing projects through the CRM API. */
 public class LiveProjectSource extends BoundedSource<GCPProject> {
   private static final long SIZE_ESTIMATE = 200;
+  private static final String DELETE_PREFIX = "DELETE";
   private String orgId;
 
   /**
@@ -224,9 +225,10 @@ public class LiveProjectSource extends BoundedSource<GCPProject> {
           orgId = project.getParent().getId();
         }
         if (project.getLifecycleState() == null
-            || !project.getLifecycleState().startsWith("DELETE")) {
-          this.projects.add(new GCPProject(project.getProjectId(), orgId, project.getName()));
+            || project.getLifecycleState().startsWith(DELETE_PREFIX)) {
+          continue;
         }
+        this.projects.add(new GCPProject(project.getProjectId(), orgId, project.getName()));
       }
       this.nextPageToken = projectListResponse.getNextPageToken();
 
